fcb0207ba7c015c9c2b119874ee15199970c89b3
Nino Miletich <nino@miletich.me>
2016-07-13 01:00:31 +0200

    add normalize css

--- lib/resume-stylist.rb
+++ lib/resume-stylist.rb
@@ -3,6 +3,9 @@ require "oga"
 require "liquid"
 require "sass"
 require "date"
+require 'net/http'
+require 'uri'
+require 'English'
 
 require "resume-stylist/version"
 require "resume-stylist/resume"
--- lib/resume-stylist/theme.rb
+++ lib/resume-stylist/theme.rb
@@ -13,7 +13,8 @@ module ResumeStylist
     end
 
     def render(resume_data)
-      @resume = @template.render(resume_data)
+      ctx = resume_data.merge({ "frontmatter" => @frontmatter })
+      @resume = @template.render(ctx)
       post_process!
 
       @resume
@@ -47,3 +48,33 @@ module ResumeStylist
     end
   end
 end
+
+class NormalizeCSS < Liquid::Tag
+
+  NormalizeCSS_URI = URI("https://necolas.github.io/normalize.css/latest/normalize.css")
+
+  def initialize(tag_name, tokens, ctx)
+    if tokens.include? "inline"
+      req = Net::HTTP::Get.new(NormalizeCSS_URI.request_uri)
+
+      http = Net::HTTP.new(NormalizeCSS_URI.host, NormalizeCSS_URI.port)
+      http.use_ssl = (NormalizeCSS_URI.scheme == "https")
+
+      response = http.request(req)
+
+      if response.code == "200"
+        @content = response.body
+      else
+        @content = "/*! ERROR: Request for `#{NormalizeCSS_URI}` returned #{response.code}! Please report this bug at https://github.com/omninonsense/resume-stylist/issues/new */"
+      end
+    else
+      @content = %Q{<link rel="stylesheet" href="#{NormalizeCSS_URI}" media="screen">}
+    end
+  end
+
+  def render(context)
+    @content
+  end
+end
+
+Liquid::Template.register_tag('normalize_css', NormalizeCSS)
